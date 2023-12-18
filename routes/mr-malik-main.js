097c9161fe8abc98883f729597b6e40fb967814d
Malik The King <130395171+Nasrullahmalik@users.noreply.github.com>
2023-12-18 21:24:49 +0500

    Update mr-malik-main.js

--- routes/mr-malik-main.js
+++ routes/mr-malik-main.js
@@ -43,7 +43,109 @@ router.get('/', cache('2 minutes'), apiRequestLimiter, function(req, res) {
 
         
 
-        let main_credit = "MR-MALIK-MD by Malik-Experts\n*_deploy now:_*\nhttps://github.com/malikexperts/MR-MALIK-MD";
+        let main_credit = "`\n🚀 *_Buckle up ${name}, Welcome To Mr-Malik! We're going on an adventure!_* 🚀\n\n╭───❮ *MR-Malik* ❯
+│ *MR-Malik*  Menu Ⓜ
+│ 
+│ _${usedPrefix}hi_
+│
+╰─────────────⦁
+\n *Follow me on Facebook:*
+facebook.com/NasrullahMachi\n
+
+╭─❮ *Tools List* ❯
+│ 🔹 *${usedPrefix}QURAN*
+│ 🔹 *${usedPrefix}CHATGPT*
+│ 🔹 *${usedPrefix}BARD*
+│ 🔹 *${usedPrefix}BING*
+│ 🔹 *${usedPrefix}IMGAI*
+│ 🔹 *${usedPrefix}REMOVEBG*
+│ 🔹 *${usedPrefix}LOGO*
+│ 🔹 *${usedPrefix}GEO*
+│ 🔹 *${usedPrefix}TOANIME*
+│ 🔹 *${usedPrefix}TOMP3*
+│ 🔹 *${usedPrefix}TOIMG*
+│ 🔹 *${usedPrefix}TOVID*
+╰─────────────⦁
+
+${readMore}
+╭───❮ *Download* ❯
+│ 🔹 *${usedPrefix}YTS*
+│ 🔹 *${usedPrefix}yts2*
+│ 🔹 *${usedPrefix}play*
+│ 🔹 *${usedPrefix}play2*
+│ 🔹 *${usedPrefix}play3*
+│ 🔹 *${usedPrefix}play4*
+│ 🔹 *${usedPrefix}playdoc*
+│ 🔹 *${usedPrefix}playdoc2*
+│ 🔹 *${usedPrefix}video*
+│ 🔹 *${usedPrefix}video2* (soon)
+│ 🔹 *${usedPrefix}insta* (soon)
+│ 🔹 *${usedPrefix}img*
+│ 🔹 *${usedPrefix}pinterest*
+│ 🔹 *${usedPrefix}mediafire*
+│ 🔹 *${usedPrefix}gdrive*
+│ 🔹 *${usedPrefix}twitter*
+│ 🔹 *${usedPrefix}tiktok*
+│ 🔹 *${usedPrefix}tiktokstalk*
+│ 🔹 *${usedPrefix}fb*
+│ 🔹 *${usedPrefix}fb2*
+│ 🔹 *${usedPrefix}apk*
+│ 🔹 *${usedPrefix}modapk*
+│ 🔹 *${usedPrefix}cricket*
+│ 🔹 *${usedPrefix}weather*
+│ 🔹 *${usedPrefix}meme*
+╰─────────────⦁
+
+╭───❮ *Convrt Audio* ❯
+│ _${usedPrefix}bass_
+│ _${usedPrefix}blown_
+│ _${usedPrefix}deep_
+│ _${usedPrefix}earrape_
+│ _${usedPrefix}fat_
+│ _${usedPrefix}fast_
+│ _${usedPrefix}nightcore_
+│ _${usedPrefix}reverse_
+│ _${usedPrefix}squrrel_
+│ _${usedPrefix}slow_
+╰─────────────⦁
+
+
+╭───❮ *Tools Sticker* ❯
+│ _${usedPrefix}sticker_
+│ _${usedPrefix}take_
+│ _${usedPrefix}smaker_
+│ _${usedPrefix}getsticker_
+│ _${usedPrefix}emix_
+│ _${usedPrefix}attp_
+╰─────────────⦁
+
+╭───❮ *Other Tools* ❯
+│ _${usedPrefix}autosticker_
+│ _${usedPrefix}whatmusic_
+│ _${usedPrefix}tempmail_
+│ _${usedPrefix}checkmail_
+│ _${usedPrefix}pokedex_
+│ _${usedPrefix}calc_
+│ _${usedPrefix}google_
+│ _${usedPrefix}lyrics_
+│ _${usedPrefix}readmore_
+│ _${usedPrefix}ssweb_
+│ _${usedPrefix}tts_
+│ _${usedPrefix}translate_
+│ _${usedPrefix}wiki_
+│ _${usedPrefix}nowa_
+│ _${usedPrefix}qrmaker_
+│ _${usedPrefix}true_
+│ _${usedPrefix}fancy_
+│ _${usedPrefix}itunes_
+╰─────────────⦁
+
+
+╭❮  *M-r Malik* ❯
+│        🦹‍♂️
+│ 💡 Created By 
+│ 📎 *NASRULLAH*
+╰────────────⦁`";
         
         var main_menu = ({
             main_credit: main_credit || "MR-MALIK-MD"
